ca77a1a6fdf01c85d91fd0afd78c6dde8699cc40
Hsoub Academy <academy@hsoub.com>
2015-09-05 21:39:57 +0100

    Add the example demo of a simple forest website

---
+++ examples/Simple-Forest-Website-Demo/js/ie-fix.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$("#header ul li:nth-child(1)").css("margin-right", "70px");
+	$("#header ul li:nth-child(2)").css("margin-right", "320px");
+	$("#header ul li:nth-child(3)").css("margin-left", "70px");
+	$("#header ul li:nth-child(4)").css("margin-left", "70px");
+});
